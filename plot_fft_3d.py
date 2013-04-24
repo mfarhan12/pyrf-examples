added WSA controls, fixed colour of initial plot, improved scaling

plot_fft_3d.py
@@ -1,58 +1,77 @@
 #!/usr/bin/python
 
+# import all the necessary Libraries
 import sys
-
-from pyqtgraph.Qt import QtCore, QtGui
-import pyqtgraph as pg
-import pyqtgraph.opengl as gl
-import scipy.ndimage as ndi
 import numpy as np
+
 from pyrf.devices.thinkrf import WSA4000
 from pyrf.util import read_data_and_reflevel
+from pyrf.config import TriggerSettings
 from pyrf.numpy_util import compute_fft
+
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.opengl as gl
 from pyqtgraph_util import represent_fft_to_plot
 from pyqtgraph_util import create_color_heatmap
 
-zSize = 128
-
+# hold a constant reflevel
 STATIC_REFLEVEL = -35
+xSize = 128
+ySize = 150
+
 dut = WSA4000()
-dut.connect('10.126.110.101')
+dut.connect(sys.argv[1])
 powerSize = dut.spp()
+
+# setup WSA settings
+dut.reset()
 dut.request_read_perm()
-## Create a GL View widget to display data
+dut.ifgain(0)
+dut.freq(2450e6)
+dut.gain('high')
+dut.fshift(0)
+dut.decimation(0)
+trigger = TriggerSettings(
+    trigtype="LEVEL",
+    fstart=2400e6,
+    fstop=2480e6,
+    amplitude=-70)
+dut.trigger(trigger)
+
+# Create a GL View widget to display data
 app = QtGui.QApplication([])
 w = gl.GLViewWidget()
 
 w.show()
-w.setWindowTitle('pyqtgraph example: GLSurfacePlot')
+w.setWindowTitle('PYRF pyqtgraph example: 3D FFT Plot')
+
 w.setCameraPosition(distance=100)
 
 # add grid
 gridSize = QtGui.QVector3D(4,5,6)
 g = gl.GLGridItem(size=gridSize)
 g.scale(5.85,5,0)
-g.setDepthValue(1)
-
 w.addItem(g)
 
-xSize = 128
-ySize = 150
-
-## Manually specified colors
+# the initial plot data before data is received
 z = np.random.normal(size=(xSize,ySize))
 
+# initialize the x-y boundries of the plot
 x = np.linspace(-73.5, 144.3, xSize)
-y = np.linspace(50, 300, ySize)
+y = np.linspace(0.1, 5, ySize)
+
+# initialize the colors of the plots (light blue)
 colors = np.ones((xSize,ySize,4), dtype=float)
 colors[:,:,0] = 0
-colors[:,:,1] = 0
+colors[:,:,1] = 0.4
 colors[:,:,2] = 1
 
-p3 = gl.GLSurfacePlotItem(z=z,x=x, y=y,colors=colors.reshape(xSize*ySize,4), shader='shaded', smooth=False)
+# plot the data
+p3 = gl.GLSurfacePlotItem(x = x, y = y, z = z, shader = 'shaded',
+                            colors = colors.reshape(xSize*ySize,4), smooth=False)
 
 # determine the size
-p3.scale(16./49., 16./49., 1.0)
+p3.scale(16./49., 20, 0.1)
 
 # determine the location on the gride
 p3.translate(-12, -50, 0)
@@ -60,17 +79,21 @@ w.addItem(p3)
 
 w.pan(-10,-10,-10)
 def update():
+    # update the plot to show new data
     global p3, z, colors
-    zSize = 128
-    powerSize = 2048
+    
+    # read data
     data, reflevel = read_data_and_reflevel(dut, powerSize)
-
-    reflevel.fields['reflevel'] = -35
+    
+    # ignore the reference level so plot doesn't change positions
+    reflevel.fields['reflevel'] = STATIC_REFLEVEL
     # compute the fft of the complex data
     powData = compute_fft(dut, data, reflevel)
     
+    # compress the FFT into a 128 array
     zData = represent_fft_to_plot(powData)
-    x = 0
+
+    # move the data stream as well as colours back to show new data
     for i in range (ySize):
         if i == 0:
             continue
@@ -79,17 +102,21 @@ def update():
             z[:,ySize - i] = z[:, ySize - i - 1]
         z[:,0] = zData[:,0]
 
+    # grab new color
     colors[:,0,:] = create_color_heatmap(powData)
     colors[:,1,:] = create_color_heatmap(powData)
-    p3.setData(z=z, colors=colors.reshape(xSize*ySize,4))
+    
+    # update the plot
+    p3.setData(z= z, colors = colors.reshape(xSize*ySize,4))
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(0.11)
+timer.start(5)
   
 ## Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
+  
   

pyqtgraph_util.py
@@ -2,6 +2,7 @@ import numpy as np
 
 PLOT_SIZE = 128
 MAX_DBM_DIFFERENCE = 60
+
 def represent_fft_to_plot(powData):
     # represent an FFT in a 128 integer array to be represented in a 3D plot
     
@@ -10,39 +11,47 @@ def represent_fft_to_plot(powData):
     for i in range(PLOT_SIZE):
         startIndex = i * (fftSize / PLOT_SIZE)
         endIndex = (i * (fftSize / PLOT_SIZE)) + ((fftSize / PLOT_SIZE) - 1)
-        zData[i] = (np.amax(powData[startIndex:endIndex]) + 100) / 10
+        zData[i] = (np.amax(powData[startIndex:endIndex]) + 100)
     
     return zData
     
 def create_color_heatmap(powData):
 # create a colored array to represent an FFT
+    
     fftSize = len(powData)
     colorArray = np.ones((PLOT_SIZE,4), dtype = float)
     powDataAverage = np.average(powData)
     powDataMin = powDataAverage - 5
     powDataMax = powDataAverage + (-1 *  powDataMin)+ MAX_DBM_DIFFERENCE
+    
     for i in range(PLOT_SIZE):
         startIndex = i * (fftSize / PLOT_SIZE)
         endIndex = (i * (fftSize / PLOT_SIZE)) + ((fftSize / PLOT_SIZE) - 1)
         fftAreaMax = np.amax(powData[startIndex:endIndex]) 
+        
+        # max data is red
         if fftAreaMax > powDataMax:
             colorArray[i,0] = 1
             colorArray[i,1] = 0
             colorArray[i,2] = 0
             continue
-        
+        # min data is blue
         elif fftAreaMax < powDataMin :
             colorArray[i,0] = 0
             colorArray[i,1] = 0
             colorArray[i,2] = 1
             continue
+        
+        # scale everything in between    
         scaledVal = fftAreaMax + (-1 * powDataMin)
         
+        # colours for lower bound (blue - green transition)
         if scaledVal < (powDataMax/2):
             colorArray[i,0] = 0
             colorArray[i,1] = 0.2 + scaledVal/powDataMax
             colorArray[i,2] = 1 - (scaledVal/powDataMax)
         
+        # colours for upper bound (green - red transition)
         elif scaledVal >= (powDataMax/2):
             colorArray[i,0] = scaledVal/powDataMax
             colorArray[i,1] = 1 - (scaledVal/powDataMax)